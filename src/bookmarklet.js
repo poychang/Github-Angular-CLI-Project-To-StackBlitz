47567d16ee0e559e9d2de3fc4469c190c539af95
Poy Chang <poypost@gmail.com>
2018-02-10 10:51:18 +0800

    fix bug

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -5,7 +5,7 @@
         pn = location.pathname;
 
     if (ln.indexOf('//github.com/') >= 0) {
-        void(location.href = ln.replace(/github\.com/i, 'stackblitz.com/github/'));
+        void(location.href = ln.replace(/github\.com/i, 'stackblitz.com/github'));
     }
     if (ln.indexOf('//stackblitz.com/github/') >= 0) {
         void(location.href = ln.replace(/stackblitz\.com\/github/i, 'github.com'));
